be150db913ce07859d8df0ed85b56c3ae0576bba
shahir <shahir@maitrise.co.in>
2024-08-01 23:16:54 +0530

    trying authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Layout from '@/components/Layout';
 import './globals.css'
-
+import NextAuthSessionProvider from '@/components/SessionProvider'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <Layout>{children}</Layout>
+        <NextAuthSessionProvider>
+          <Layout>{children}</Layout>
+        </NextAuthSessionProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-
 import HomePage from '../components/HomePage';
+import AuthButton from '@/components/AuthButton';
 import './globals.css'
 
 export default function Home() {
   return (
-    
+    <div>
       <HomePage />
-    
+      <AuthButton />
+    </div>
   );
 }
